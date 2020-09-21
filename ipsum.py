a3f2e0ff8a4488ca7938697a563d11660df85e19
impshum <impshum@impshum.co.uk>
2020-09-21 22:15:49 +0100

    Update ipsum.py

--- ipsum.py
+++ ipsum.py
@@ -1,5 +1,13 @@
-#!/usr/bin/python
-import urllib, pyperclip
-a = urllib.urlopen('http://loripsum.net/api/').read()
-pyperclip.copy(a)
-b = pyperclip.paste()
+from requests import get
+import pyperclip
+import argparse
+
+
+parser = argparse.ArgumentParser(description='IPS v2 (by u/impshum)')
+parser.add_argument('length', nargs='?', default=1, type=int)
+args = parser.parse_args()
+
+url = f'https://loripsum.net/api/plaintext/{args.length}'
+data = get(url).text.strip()
+pyperclip.copy(data)
+pyperclip.paste()
